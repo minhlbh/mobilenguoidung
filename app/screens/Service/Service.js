2c99da9b5bb61c81362b425ed0d96a9cafbb8c3f
Le Duc Tien <leductien1997123@gmail.com>
2017-09-14 00:12:39 +0700

    Form

--- app/screens/Service/Service.js
+++ app/screens/Service/Service.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {
-    Container, Header, Left, Body, Right, Content,
+    Container, Header, Left, Body, Right, Content, ListItem, Thumbnail,
     Button,
     Icon,
     Title,
@@ -9,9 +9,10 @@ import {
     Item,
     Input,
     Label,
-    Text
+    Text,
+    View
 } from 'native-base';
-import {AsyncStorage} from 'react-native';
+import { AsyncStorage } from 'react-native';
 import styles from './styles';
 import serviceApi from '../../api/serviceApi';
 import signalr from 'react-native-signalr';
@@ -20,31 +21,41 @@ import SignalService from '../../Share/SignalService';
 
 
 class Service extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
-          selectedHoso: undefined,
-          isAnonymous : true,
-          DsHoSo: [],
-          picture: '',
-          vanDe: '',
+            selectedHoso: undefined,
+            isAnonymous: true,
+            DsHoSo: [],
+            picture: '',
+            vanDe: '',
+            name: '',
+            birth: '',
+            gender: '',
+
         };
         AsyncStorage.getItem('access_token').then((value) => {
             console.log(value);
             serviceApi.getProfiles(value).then((res) => this.setState(
-                 { DsHoSo: res.accountSoYBa.DsHoSoSucKhoe}
+                {
+                    DsHoSo: res.accountSoYBa.DsHoSoSucKhoe,
+                    avatar: res.accountSoYBa.DsHoSoSucKhoe.Avatar,
+                    name: res.accountSoYBa.DsHoSoSucKhoe.HoVaTen,
+                    birth: res.accountSoYBa.DsHoSoSucKhoe.NgaySinh,
+                    gender: res.accountSoYBa.DsHoSoSucKhoe.GioiTinh
+                }
             ))
         });
 
-        const {state} = this.props.navigation;
+        const { state } = this.props.navigation;
         SignalService.proxy.on('timBacSi_KetQua', (KetQua, UserId) => {
             console.log('message-from-server', KetQua, UserId);
-            if(!UserId){
+            if (!UserId) {
                 alert(KetQua)
-            }else{
+            } else {
                 this.props.navigation.navigate('DoctorInfo', {
-                    ketQua:KetQua,
+                    ketQua: KetQua,
                     idDichVu: state.params.id,
                     doctorId: UserId
                 });
@@ -53,56 +64,56 @@ class Service extends Component {
         });
     }
 
-    findDoctor(){
+    findDoctor() {
         var id = this.props.navigation.state.params.id;
         var idHoSo = this.state.selectedHoso;
         var isAnonymous = this.state.isAnonymous;
         var vanDe = this.state.vanDe;
         var linkAnh = this.state.picture;
-        
-      
-        SignalService.proxy.invoke('timBacSi', id,idHoSo,isAnonymous,vanDe,linkAnh)
-        .done((directResponse) => {
-            console.log('direct-response-from-server', directResponse);
-           
-        }).fail(() => {
-            console.warn('Something went wrong when calling server, it might not be up and running?')
-        });
-       
+
+
+        SignalService.proxy.invoke('timBacSi', id, idHoSo, isAnonymous, vanDe, linkAnh)
+            .done((directResponse) => {
+                console.log('direct-response-from-server', directResponse);
+
+            }).fail(() => {
+                console.warn('Something went wrong when calling server, it might not be up and running?')
+            });
+
 
         // const connection = signalr.hubConnection('http://admincloud.truongkhoa.com/SignalR');
         // connection.logging = true;
-    
+
         // const proxy = connection.createHubProxy('truongKhoaHub');
         // //receives broadcast messages from a hub function, called "helloApp"
         // proxy.on('timBacSi_KetQua', (KetQua, UserId) => {
         //   console.log('message-from-server', KetQua, UserId);
         //   //Here I could response by calling something else on the server...
         // });
-    
+
         // // atempt connection, and handle errors
         // connection.start().done(() => {
         //     var notify = new EventEmitter();
         //     notify.emit('daKetNoi','Đã kết nối thành công');
 
         //   console.log('Now connected, connection ID=' + connection.id);
-    
+
         //   proxy.invoke('timBacSi', id,idHoSo,isAnonymous,vanDe,linkAnh)
         //     .done((directResponse) => {
         //       console.log('direct-response-from-server', directResponse);
         //     }).fail(() => {
         //       console.warn('Something went wrong when calling server, it might not be up and running?')
         //     });
-    
+
         // }).fail(() => {
         //     console.log('Failed');
         // });
-    
+
         // //connection-handling
         // connection.connectionSlow(() => {
         //      console.log('We are currently experiencing difficulties with the connection.')
         // });
-    
+
         // connection.error((error) => {
         //     const errorMessage = error.message;
         //     let detailedError = '';
@@ -117,9 +128,9 @@ class Service extends Component {
     }
 
     onValueChange(value) {
-        const {state} = this.props.navigation;
-        if(value == 'taohoso'){
-            this.props.navigation.navigate('TaoNhanhHoSo',{
+        const { state } = this.props.navigation;
+        if (value == 'taohoso') {
+            this.props.navigation.navigate('TaoNhanhHoSo', {
                 id: state.params.id,
                 name: state.params.name
             });
@@ -134,60 +145,78 @@ class Service extends Component {
             isAnonymous: value
         });
     }
-    render(){
-        const {state} = this.props.navigation;
-        return(
+    componentWillMount() {
+
+    }
+    render() {
+        const { state } = this.props.navigation;
+        return (
             <Container>
-                <Header>
+                <Header style={styles.header}>
                     <Left>
                         <Button transparent onPress={() => this.props.navigation.navigate('Tabs')}>
                             <Icon name="ios-arrow-back" />
                         </Button >
                     </Left>
                     <Body>
-                        <Title>{state.params.name}</Title>
+                        <Title style={styles.textHeader}>{state.params.name}</Title>
                     </Body>
-                    <Right/>
+                    <Right />
                 </Header>
                 <Content>
-                <Form style= {styles.form}>
-                    <Picker
-                        mode="dropdown"
-                        placeholder="Select One"
-                        selectedValue={this.state.selectedHoso}
-                        onValueChange={this.onValueChange.bind(this)}
-                    >
-                        <Item label="Chọn hồ sơ" value="key0" />
-                        <Item label="Tạo nhanh hồ sơ mới" value="taohoso" />
-                    {this.state.DsHoSo.map((item) => (
-                        <Item label={item.HoVaTen} value={item.Id}/>
-                    ))}
-                    </Picker>
-                    <Picker
-                        style= {{marginTop: 10}}
-                        mode="dropdown"
-                        placeholder="Select One"
-                        selectedValue={this.state.isAnonymous}
-                        onValueChange={this.onValueChange2.bind(this)}
-                    >
-                        <Item label="Ẩn danh khi gặp bác sĩ" value={true} />
-                        <Item label="Xưng danh khi gặp bác sĩ" value={false} />
-                    </Picker>
+                        <View style={styles.panel1}>
+                            <ListItem>
+                                {this.state.avatar ?
+                                    <Thumbnail large source={{ uri: this.state.avatar }}
+                                    /> :
+                                    <Thumbnail large style={{ marginRight: 20, marginLeft: 20 }} source={{ uri: 'http://www.unl.edu/careers/images/staff_images/y_u_no_photo_Square.png' }}
+                                    />
+                                }
+                                <Body>
+                                    <Picker style={styles.picker1}
+                                        mode="dropdown"
+                                        placeholder="Select One"
+                                        selectedValue={this.state.selectedHoso}
+                                        onValueChange={this.onValueChange.bind(this)}
+                                    >
+                                        <Item label="Chọn hồ sơ" value="key0" />
+                                        <Item label="Tạo nhanh hồ sơ mới" value="taohoso" />
+                                        {this.state.DsHoSo.map((item) => (
+                                            <Item label={item.HoVaTen} value={item.Id} />
+                                        ))}
+                                    </Picker>
+                                    <Text>Họ và tên: {this.state.name}</Text>
+                                    <Text>Ngày sinh: {this.state.birth}</Text>
+                                    <Text>Giới tính: {this.state.gender}</Text>
+                                </Body>
 
-                    <Item regular style= {{marginTop: 10}} >
-                        <Input style={{height: 200}}
-                         multiline={true} 
-                         placeholder='Nhập vấn đề bạn gặp phải' 
-                         onChangeText={(vanDe) => this.setState({ vanDe })}
-                        />
-                    </Item>
+                            </ListItem>
+                            <Picker
+                                style={styles.picker2}
+                                mode="dropdown"
+                                placeholder="Select One"
+                                selectedValue={this.state.isAnonymous}
+                                onValueChange={this.onValueChange2.bind(this)}
+                            >
+                                <Item label="Ẩn danh khi gặp bác sĩ" value={true} />
+                                <Item label="Xưng danh khi gặp bác sĩ" value={false} />
+                            </Picker>
+                        </View>
+                        <Item regular style={{ marginTop: 10 }} >
+                            <View>
+                                <Text style={styles.text}>Lời nhắn tới bác sĩ</Text>
+                                <Input
+                                    multiline={true}
+                                    onChangeText={(vanDe) => this.setState({ vanDe })}
+                                />
+                            </View>
+                        </Item>
 
-                    <Button full primary style={{marginTop: 10}}
-                        onPress={()=> this.findDoctor()}
-                    >
-                        <Text> Tìm bác sĩ </Text>
-                    </Button>
-                </Form>
+                        <Button full primary style={{ marginTop: 10 }}
+                            onPress={() => this.findDoctor()}
+                        >
+                            <Text> Tìm bác sĩ </Text>
+                        </Button>
                 </Content>
             </Container>
         )
--- app/screens/Service/styles.js
+++ app/screens/Service/styles.js
@@ -2,8 +2,29 @@ import { StyleSheet } from 'react-native';
 import { colors } from '../../config/styles';
 
 export default StyleSheet.create({
-    form:{
-        marginTop: 10, 
-        padding: 10
+    header:{
+        backgroundColor: colors.header.background,
+    },
+    textHeader:{
+        color: colors.header.text
+    },
+    panel1:{
+        backgroundColor: '#0c879a'
+    },
+    picker1:{
+        width:  200,
+        backgroundColor:'white',
+        height:35,
+        marginBottom: 20
+    },
+    picker2:{
+        backgroundColor:'white',
+        width: 300,
+        alignSelf:'center',
+        height: 35,
+        marginTop:5
+    },
+    text:{
+        backgroundColor: colors.header.background,
     }
 })
